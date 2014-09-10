2431dac4f97b7caf472376343546d5aedb52fab4
Rasmus Jürs <rasmus@jurs.me>
2014-09-10 16:09:54 +0200

    Refine and simplify typography system

--- 404.php
+++ 404.php
@@ -9,8 +9,10 @@
 
 				<h1><?php _e( 'Page not found', 'secondthought' ); ?></h1>
 				<h2>
-					<a href="<?php echo home_url(); ?>"><?php _e( 'Return home?', 'secondthought' ); ?></a>
+					<a class="button" href="<?php echo home_url(); ?>"><?php _e( 'Return home?', 'secondthought' ); ?></a> or
 				</h2>
+
+				<?php get_template_part('searchform') ?>
 
 			</article>
 			<!-- /article -->
