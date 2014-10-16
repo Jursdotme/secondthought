0071ea0c8a377967f85554461cf4754ac1826dc5
Rasmus Jürs <rasmus@jurs.me>
2014-10-16 14:37:47 +0200

    Add danish localization

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 		<!-- section -->
 		<section>
 
-			<h1><?php _e( 'Latest Posts', 'secondthought' ); ?></h1>
+			<h1><?php _e( 'Latest News', 'secondthought' ); ?></h1>
 
 			<?php get_template_part('partials/loop'); ?>
 
