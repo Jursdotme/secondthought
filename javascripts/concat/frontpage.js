eb1be60cf68580135e06a641251f1770a26e4247
Rasmus Jürs <rasmus@jurs.me>
2015-03-24 16:59:33 +0100

    Add js to remove all empty paragraph tags

--- javascripts/concat/frontpage.js
+++ javascripts/concat/frontpage.js
@@ -12,3 +12,12 @@ $(document).ready(function(){
     // Find options here: http://kenwheeler.github.io/slick/
   });
 });
+
+// Remove all empty <p> tags
+$(document).ready(function(){
+  $('p').each(function() {
+    var $this = $(this);
+    if($this.html().replace(/\s|&nbsp;/g, '').length == 0)
+        $this.remove();
+  });
+});
