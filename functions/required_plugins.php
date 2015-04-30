4b60b7ca19df36c91d8dbb4844d1b7b987272b9e
Rasmus Jürs <rasmus@jurs.me>
2015-04-30 14:01:49 +0200

    Fix doubble code in TGM

--- functions/required_plugins.php
+++ functions/required_plugins.php
@@ -90,14 +90,6 @@ function my_theme_register_required_plugins() {
             'slug'      => 'contact-form-7',
             'required'  => false,
         ),
-        // This is an example of how to include a plugin from a private repo in your theme.
-        array(
-            'name'               => 'Secondthought Widget Pack', // The plugin name.
-            'slug'               => 'secondthought-pagebuilder-bundle', // The plugin slug (typically the folder name).
-            'source'             => 'https://github.com/Jursdotme/secondthought-pagebuilder-bundle/archive/1.0.0.zip', // The plugin source.
-            'required'           => true, // If false, the plugin is only 'recommended' instead of required.
-            'external_url'       => 'https://github.com/Jursdotme/secondthought-pagebuilder-bundle/archive/1.0.0.zip', // If set, overrides default API URL and points to an external URL.
-        ),
 
         array(
       			'name'      => 'Secondthought Widget Pack',
