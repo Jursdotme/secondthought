6bad2d454972690194cf3b27776f9e7690740bf6
Rasmus Jürs <rasmus@jurs.me>
2015-06-11 11:31:29 +0200

    Non customizer header

---
+++ partials/page_header.php
@@ -0,0 +1,21 @@
+<!-- header -->
+
+<div class="header_wrapper">
+  <header class="header clear" role="banner">
+    <div class="header-inner nav-right">
+
+      <div class='logo'>
+        <a href='<?php echo home_url(); ?>'>
+          <img src='<?php echo get_template_directory_uri(); ?>/img/logo.svg' alt='Logo' class='logo-img'>
+        </a>
+      </div>
+
+      <?php secondthought_nav(); ?>
+
+    </div>
+  </header>
+</div>
+
+<!-- /header -->
+
+<?php get_template_part('partials/mobile_navigation'); ?>
