b3c7ff31c0a46b7450866484e5d94e38adcb5d7e
Rasmus Jürs <rasmus@jurs.me>
2015-09-24 15:31:48 +0200

    Update footer widget area

--- footer.php
+++ footer.php
@@ -1,46 +1,49 @@
 			<!-- footer -->
-			<div class="footer-container">
+			<footer class="footer-container">
 
 				<?php if ( is_active_sidebar( 'footer-widget-area' ) ) { ?>
-						<?php dynamic_sidebar( 'footer-widget-area' ); ?>
+						<div class="footer-widgets">
+							<?php dynamic_sidebar( 'footer-widget-area' ); ?>
+						</div>
 				<?php } ?>
 
 				<?php if (!get_field('skjul_footer_addresselinie','option')) { ?>
 
+				<div class="footer-address-bar-wrapper">
+					<div class="footer-address-bar" role="contentinfo">
 
-				<footer class="footer" role="contentinfo">
+						<?php
 
-					<?php
+							$firmanavn      = get_field('firmanavn', 'option');
+							$adresse        = get_field('adresse', 'option');
+							$postnummer     = get_field('postnummer', 'option');
+							$by             = get_field('by', 'option');
+							$telefonnummer  = get_field('telefonnummer', 'option');
+							$fax            = get_field('fax', 'option');
+							$faxEnabled     = get_field('vi_har_fax', 'option');
+							$email_addresse = get_field('email_addresse', 'option');
 
-						$firmanavn      = get_field('firmanavn', 'option');
-						$adresse        = get_field('adresse', 'option');
-						$postnummer     = get_field('postnummer', 'option');
-						$by             = get_field('by', 'option');
-						$telefonnummer  = get_field('telefonnummer', 'option');
-						$fax            = get_field('fax', 'option');
-						$faxEnabled     = get_field('vi_har_fax', 'option');
-						$email_addresse = get_field('email_addresse', 'option');
+							if($faxEnabled) {
+								$insertFax = "Fax: " . $fax . "<hr>";
+							} else {
+								$insertFax = "";
+							};
+						?>
 
-						if($faxEnabled) {
-							$insertFax = "Fax: " . $fax . "<hr>";
-						} else {
-							$insertFax = "";
-						};
-					?>
-
-					<div class="footer-address">
-						<?php if($firmanavn) { echo '<span class="firmanavn">' . $firmanavn . '</span><hr>';} ?>
-						<?php if($adresse) { echo '<span class="adresse">' . $adresse . '</span><hr>';} ?>
-						<?php if($postnummer) { echo '<span class="postnr-by">' . $postnummer;} ?>
-						<?php if($by) { echo $by . '</span><hr>';} ?>
-						<?php if($telefonnummer) { echo '<span class="tlf">Tlf.: ' . $telefonnummer . '</span><hr>'; } ?>
-						<a href="mailto:<?php echo $email_addresse; ?>"><?php echo $email_addresse; ?></a>
+						<div class="footer-address">
+							<?php if($firmanavn) { echo '<span class="firmanavn">' . $firmanavn . '</span><hr>';} ?>
+							<?php if($adresse) { echo '<span class="adresse">' . $adresse . '</span><hr>';} ?>
+							<?php if($postnummer) { echo '<span class="postnr-by">' . $postnummer;} ?>
+							<?php if($by) { echo $by . '</span><hr>';} ?>
+							<?php if($telefonnummer) { echo '<span class="tlf">Tlf.: ' . $telefonnummer . '</span><hr>'; } ?>
+							<a href="mailto:<?php echo $email_addresse; ?>"><?php echo $email_addresse; ?></a>
+						</div>
 					</div>
-				</footer>
+				</div>
 
 				<?php } ?>
 
-			</div>
+			</footer>
 			<!-- /footer -->
 
 		</div>
--- functions/widget-areas.php
+++ functions/widget-areas.php
@@ -1,27 +1,28 @@
 <?php // If Dynamic Sidebar Exists
 if (function_exists('register_sidebar'))
 {
-    // Define Sidebar Footer Widget Area 1
-    register_sidebar(array(
-        'name'          => __('Sidebar Widget Area', 'secondthought'),
-        'description'   => __('Widget area defined in the sidebar. If sidebar is not used this area will have no effect.', 'secondthought'),
-        'id'            => 'sidebar-widget-area',
-        'before_widget' => '<div id="%1$s" class="%2$s widget-area">',
-        'after_widget'  => '</div>',
-        'before_title'  => '<h3>',
-        'after_title'   => '</h3>'
-    ));
+  // Define Footer Widget Area
+  register_sidebar(array(
+      'name'          => __('Footer Widget Area', 'secondthought'),
+      'description'   => __('Widgets in the footer', 'secondthought'),
+      'id'            => 'footer-widget-area',
+      'before_widget' => '<div id="%1$s" class="%2$s widget-area">',
+      'after_widget'  => '</div>',
+      'before_title'  => '<h3>',
+      'after_title'   => '</h3>'
+  ));
 
-    // Define Sidebar Footer Widget Area 1
-    register_sidebar(array(
-        'name'          => __('Footer Widget Area', 'secondthought'),
-        'description'   => __('Widgets in the footer', 'secondthought'),
-        'id'            => 'footer-widget-area',
-        'before_widget' => '<div id="%1$s" class="%2$s widget-area">',
-        'after_widget'  => '</div>',
-        'before_title'  => '<h3>',
-        'after_title'   => '</h3>'
-    ));
+  // register_sidebar(array(
+  //     'name'          => __('Sidebar Widget Area', 'secondthought'),
+  //     'description'   => __('Widget area defined in the sidebar. If sidebar is not used this area will have no effect.', 'secondthought'),
+  //     'id'            => 'sidebar-widget-area',
+  //     'before_widget' => '<div id="%1$s" class="%2$s widget-area">',
+  //     'after_widget'  => '</div>',
+  //     'before_title'  => '<h3>',
+  //     'after_title'   => '</h3>'
+  // ));
+
+
 
 
 } ?>
