5f6b3352b9ea6611dcae5b9a3fc920dede2eff50
Rasmus Jürs <rasmus@jurs.me>
2015-11-26 10:41:24 +0100

    Add local jQuery if logged in as admin

--- functions/styles-scripts-loader.php
+++ functions/styles-scripts-loader.php
@@ -3,18 +3,23 @@
 //////////////////////////////////////////////////////////////////////
 // Load Secondthought footer scripts (footer.php)
 
-
 function secondthought_header_scripts()
 {
     if ($GLOBALS['pagenow'] != 'wp-login.php' && !is_admin()) {
 
       wp_deregister_script( 'jquery' ); // Deregister WordPress jQuery
+      
+      if (current_user_can('activate_plugins')) {
+        wp_register_script('jquery', get_template_directory_uri() . '/javascripts/lib/jquery-2.1.4.min.js', array(), '1.0.0', true); // Custom scripts
+        wp_enqueue_script('jquery'); // Enqueue it!
+      } else {
+        wp_register_script('jquery', "http" . ($_SERVER['SERVER_PORT'] == 443 ? "s" : "") . "://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js", array(), '2.1.4', true); // jQuery
+        wp_enqueue_script('jquery'); // Enqueue it!
+      }
 
       wp_register_script('modernizr', "https://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.min.js", 'jquery', '2.8.3', false); // jQuery
       wp_enqueue_script('modernizr'); // Enqueue it!
 
-        wp_register_script('jquery', "http" . ($_SERVER['SERVER_PORT'] == 443 ? "s" : "") . "://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js", array(), '1.11.0', true); // jQuery
-        wp_enqueue_script('jquery'); // Enqueue it!
 
         wp_register_script('secondthoughtscripts', get_template_directory_uri() . '/build/scripts/all.min.js', array('jquery'), '1.0.0', true); // Custom scripts
         wp_enqueue_script('secondthoughtscripts'); // Enqueue it!
@@ -23,10 +28,8 @@ function secondthought_header_scripts()
 
 add_action('init', 'secondthought_header_scripts'); // Add Custom Scripts to wp_head
 
-
 //////////////////////////////////////////////////////////////////////
 // Load Secondthought conditional scripts
-
 
 function secondthought_conditional_scripts()
 {
