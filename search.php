149d564f67ff3720f043e8eaca2587577d0c9dd5
Rasmus Jürs <rasmus@jurs.me>
2016-06-09 17:02:32 +0200

    Add missing space in search page header

--- search.php
+++ search.php
@@ -5,7 +5,7 @@
 		<!-- section -->
 		<section>
 
-			<h1><?php echo sprintf( __( '%s Search Results for ', 'secondthought' ), $wp_query->found_posts ); echo get_search_query(); ?></h1>
+			<h1><?php echo sprintf( __( '%s Search Results for ', 'secondthought' ), $wp_query->found_posts ); echo " " . get_search_query(); ?></h1>
 
 			<?php get_template_part('partials/loop'); ?>
 
