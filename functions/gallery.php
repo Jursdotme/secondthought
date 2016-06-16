5e92d47d1ab7cf4485011680b57ce58f8e35ca5e
Rasmus Jürs <rasmus@jurs.me>
2016-06-16 13:51:57 +0200

    Update gallery

--- functions/gallery.php
+++ functions/gallery.php
@@ -4,7 +4,7 @@
 add_filter( 'use_default_gallery_style', '__return_false' );
 
 // Remove clearing <br> tags from markup
-// add_filter( 'the_content', 'remove_br_gallery', 11, 2);
+add_filter( 'the_content', 'remove_br_gallery', 11, 2);
 function remove_br_gallery($output) {
     return preg_replace('/\<br[^\>]*\>/','',$output);
 }
--- javascripts/concat/gallery.js
+++ javascripts/concat/gallery.js
@@ -11,21 +11,6 @@ $(document).ready(function() {
     closeBtn: true,
   });
 
-  $('.gallery').each( function(){
-
-    $this = $(this);
-
-    element1 = $this.find('.gallery-item:eq(0)');
-    element2 = $this.find('.gallery-item:eq(1)');
-    element1Width = element1.outerWidth();
-
-    margin = element2.position().left-(element1.position().left + element1Width);
-
-    $this.find('.gallery-item').css('margin-bottom', margin);
-  });
-
-  $('dl.gallery-item').matchHeight();
-
   $('.gallery br').remove();
 
 });
