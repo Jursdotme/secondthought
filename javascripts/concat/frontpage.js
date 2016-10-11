459cc6756813616ff6e7a2b893ecc01fa5b39131
Rasmus Jürs <rasmus@jurs.me>
2016-10-11 11:24:07 +0200

    New responsive table solution

--- javascripts/concat/frontpage.js
+++ javascripts/concat/frontpage.js
@@ -4,11 +4,34 @@ $(document).ready(function(){
   });
 });
 
-// Remove all empty <p> tags
 $(document).ready(function(){
+
+  // Remove all empty <p> tags
   $('p').each(function() {
     var $this = $(this);
     if($this.html().replace(/\s|&nbsp;/g, '').length === 0)
         $this.remove();
   });
+
+  // Responsive tables
+  $('table').each(function(){
+
+    var $this = $(this);
+    var tableHeaders = [];
+
+    $this.find('th').each( function(){
+      tableHeaders.push( $(this).html() );
+    });
+
+    $this.find('tr').each(function(){
+      $(this).find('td').each(function(index){
+        if($(this).html()==="") {
+           $(this).addClass('hidden');
+        }
+        $(this).attr('aria-label', tableHeaders[index] );
+      });
+    });
+
+  });
+
 });
