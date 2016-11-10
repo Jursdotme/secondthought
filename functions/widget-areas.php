a813f06af9a780d952e068aecf5cf39109681bcd
Rasmus Jürs <rasmus@jurs.me>
2016-11-10 14:11:17 +0100

    Add shared content

--- functions/widget-areas.php
+++ functions/widget-areas.php
@@ -12,4 +12,19 @@ if (function_exists('register_sidebar'))
       'after_title'   => '</h3>'
   ));
 
-} ?>
+  if( have_rows('widget_omrade', 'option') ):
+    while ( have_rows('widget_omrade', 'option') ) : the_row();
+
+      register_sidebar(array(
+          'name'          => __(get_sub_field('name'), 'secondthought'),
+          'description'   => "",
+          'id'            => sanitize_title( get_sub_field('description') ) . '-widget-area',
+          'before_widget' => '<div id="%1$s" class="%2$s widget-area">',
+          'after_widget'  => '</div>',
+          'before_title'  => '<h3>',
+          'after_title'   => '</h3>'
+      ));
+
+    endwhile;
+  endif;
+}
