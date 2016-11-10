d1cdf1e91c8046565945402e1cc5b4bbbc4ff3ab
Rasmus Jürs <rasmus@jurs.me>
2016-11-10 13:11:01 +0100

    Remove unused sidebar

--- functions/widget-areas.php
+++ functions/widget-areas.php
@@ -12,14 +12,4 @@ if (function_exists('register_sidebar'))
       'after_title'   => '</h3>'
   ));
 
-  // register_sidebar(array(
-  //     'name'          => __('Pagebuilder Widget Area', 'secondthought'),
-  //     'description'   => __('', 'secondthought'),
-  //     'id'            => 'pagebuilder-widget-area',
-  //     'before_widget' => '<div id="%1$s" class="%2$s widget-area">',
-  //     'after_widget'  => '</div>',
-  //     'before_title'  => '<h3>',
-  //     'after_title'   => '</h3>'
-  // ));
-
 } ?>
