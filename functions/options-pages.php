9e288d6a609ad1c384207441eae46c307edbd156
Rasmus Jürs <rasmus@jurs.me>
2016-11-10 14:11:57 +0100

    add admin options page and make options pages translatable

--- functions/options-pages.php
+++ functions/options-pages.php
@@ -1,12 +1,22 @@
 <?php if( function_exists('acf_add_options_page') ) {
 
 	acf_add_options_page(array(
-		'page_title' 	=> 'Sideoplysninger',
-		'menu_title'	=> 'Sideoplysninger',
+		'page_title' 	=> __('Page Options', 'secondthought'),
+		'menu_title'	=> __('Page Options', 'secondthought'),
 		'menu_slug' 	=> 'theme-general-settings',
 		'capability'	=> 'edit_posts',
 		'redirect'		=> false,
-    'icon_url'    => 'dashicons-info',
+    'icon_url'    => 'dashicons-admin-generic',
+    'position'    => '59.9'
+	));
+
+	acf_add_options_page(array(
+		'page_title' 	=> __('Admin Options', 'secondthought'),
+		'menu_title'	=> __('Admin Options', 'secondthought'),
+		'parent_slug'	=> 'theme-general-settings',
+		'capability'	=> 'edit_posts',
+		'redirect'		=> false,
+    'icon_url'    => 'dashicons-admin-settings',
     'position'    => '59.9'
 	));
 
